de544c5db29d90abc73842804c3cc270fd90c332
David Camargo <davidesteban3570@gmail.com>
2026-05-26 18:31:17 -0500

    Agrega metodos de multiplicacion y division

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,10 @@
 publica class Calculator {
 
+        public int multiply(int a, intb) {
+           return a*b;
+      }
+
+        public int divide(int a, int b) { 
+           return a / b;
+      }
 }
